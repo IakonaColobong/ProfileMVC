94d7e3c54089d852213e73d971809ef1e10fda6c
IakonaColobong <jason.colobong@outlook.com>
2022-02-23 22:18:58 -0600

    added downloadable resume. removed unsused contact page from shared layout

--- Profile.MVC.UI/Controllers/HomeController.cs
+++ Profile.MVC.UI/Controllers/HomeController.cs
@@ -1,60 +1,88 @@
-﻿using System;
+﻿using Profile.MVC.UI.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace Profile.MVC.UI.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        //[HttpGet]
+        //[Authorize]
         //public ActionResult About()
         //{
-        //    ViewBag.Message = "Your application description page.";
+        //    ViewBag.Message = "Your app description page.";
 
         //    return View();
         //}
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
-        //Commented out until I decide if a multi page app would be more appealing
-        //public ActionResult GitHub()
-        //{
-        //    ViewBag.Message = "GitHub";
-        //    return View();
-        //}
-        //public ActionResult CSharp()
-        //{
-        //    ViewBag.Message = "Text Adventure";
-        //    return View();
-        //}
 
-        //public ActionResult StoreFront()
-        //{
-        //    ViewBag.Message = "Store Front";
-        //    return View();
-        //}
-        //public ActionResult StaticHTML()
-        //{
-        //    ViewBag.Message = "HTML/CSS Only site";
-        //    return View();
-        //}
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+            return View(cvm);
 
-        //public ActionResult Portfolio()
-        //{
-        //    ViewBag.Message = "Project Portfolio";
-        //    return View();
-        //}
+            }
+            string body = $"{cvm.Name} Sent you the following message: <br /> " +
+                    $"{cvm.Message} <strong>from the email address: </strong> {cvm.Email}";
+
+                MailMessage mm = new MailMessage(
+
+                    ConfigurationManager.AppSettings["EmailUser"].ToString(),
+
+                    ConfigurationManager.AppSettings["EmailTo"].ToString(),
+
+                    cvm.Subject,
+                    body);
 
 
+                mm.IsBodyHtml = true;
+                mm.Priority = MailPriority.High;
+                mm.ReplyToList.Add(cvm.Email);
+
+                SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+                client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.CustomerMessage = $"We're sorry your request could not be completed at this time. Please try again later. Error Message:<br/>{ex.StackTrace}";
+                return View(cvm);
+                }
+
+                //send the cvm back to the form with completed inputs
+                return View("EmailConfirmation", cvm);
+
+          
+        }
+
+
+           
     }
 }
---
+++ Profile.MVC.UI/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //brought in to support email
+using Profile.MVC.UI;
+
+namespace Profile.MVC.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Your name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "* Your E-mail is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "* A subject heading is required")]
+        public string Subject { get; set; }
+
+        [UIHint("MultiLineText")]
+        [Required(ErrorMessage = "* A message is required")]
+        public string Message { get; set; }
+    }
+}
